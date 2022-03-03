8081f427be24d7b14546a1c39f4af1cb0fa57435
= <=>
2022-03-03 22:28:55 +0700

    first commit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <h1>Bonafide Store</h1>
+        </div>
+    </div>
+@endsection
